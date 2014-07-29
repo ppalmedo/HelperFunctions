40a745c2ceeba6bc61dcd9e7f1646c8c95753e1a
ppalmedo <ppalmedo@perrys-mbp.dfci.harvard.edu>
2014-07-29 16:38:27 -0400

    updated PythonHelpers

--- PythonHelpers/helperFunc.py
+++ PythonHelpers/helperFunc.py
@@ -1,9 +1,11 @@
 '''
-Created on Jul 29, 2014
+helper Func: Created July 2014
+    helper Func contains helper functions for Python.  Developed primarily for bioinformatic purposes.
 
 @author: ppalmedo
 '''
 
+# function to take two lists (i.e. sample or genes) and return indices for them
 def findOLIndices(listA,listB):
     indexList = [(listA.index(g), listB.index(g)) for g in listA if g in listB]
     iA, iB = map(list, zip(*indexList))
